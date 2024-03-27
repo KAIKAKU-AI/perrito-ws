775bde8e7f31c2679bbe0cae6ed232b30d834def
Tinos <46272051+pTinosq@users.noreply.github.com>
2024-03-27 18:01:29 +0000

    Add React JSX support and create app component

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import { createRoot } from 'react-dom/client'
+
+const root = createRoot(document.body)
+root.render(
+  <>
+    <h1>💖 Hello World!</h1>
+    <p>Welcome to Perrito</p>
+  </>,
+)
--- src/renderer.ts
+++ src/renderer.ts
@@ -26,6 +26,8 @@
  * ```
  */
 
-import './index.css'
+import './index.css';
 
 console.log('👋 This message is being logged by "renderer.ts", included via Vite')
+
+import './app';
