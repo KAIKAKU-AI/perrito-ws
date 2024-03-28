7a297960f56e9bf5c3e86f426c6f429d84bfb25a
Tinos <46272051+pTinosq@users.noreply.github.com>
2024-03-28 15:14:33 +0000

    Updated window sizes

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,8 @@ if (require('electron-squirrel-startup')) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 720,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
