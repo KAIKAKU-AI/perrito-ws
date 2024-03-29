e5f74bbac9aed9c3a636f378b2e1f7cd0cd2eb3d
ptinosq <tinosps@gmail.com>
2024-03-29 12:25:13 +0000

    Update Header and SideBar styles

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 import SideBarControllerButton from './SideBarControllerButton'
 import './styles.scss'
 
-interface indexProps {}
+interface indexProps {
+  title?: string
+  children?: React.ReactNode
+}
 
 const index = (props: indexProps) => {
   const [open, setOpen] = useState(true)
 
   return (
     <div className={`sidebar sidebar--${open ? 'open' : 'closed'}`}>
-      <div className={`sidebar-content sidebar-content--${open ? 'open' : 'closed'}`}></div>
       <div className="sidebar-controller__container">
         <div className="sidebar-controller__button-container">
           <SideBarControllerButton
@@ -17,6 +19,12 @@ const index = (props: indexProps) => {
             defaultArrowDirection={open ? 'center' : 'right'}
             hoverArrowDirection={open ? 'left' : 'right'}
           />
+        </div>
+      </div>
+      <div className={`sidebar-content`}>
+        <div className="sidebar-content__title">{props.title}</div>
+        <div className="sidebar-content__content">
+          <div className="sidebar-content__content-inner">{props.children}</div>
         </div>
       </div>
     </div>
--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -10,7 +10,7 @@ const DashboardPage = (props: DashboardPageProps) => {
       <Header activePage="dashboard" />
       <div id="page-content">
         <div className="sidebar-container">
-          <SideBar />
+          <SideBar title="Connections" />
         </div>
       </div>
     </>
